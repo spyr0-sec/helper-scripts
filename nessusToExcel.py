5fa5ccf7a52c23afbfcec1f4c52b1ce0d4c2a2b7
lapolis_aka_blu <57040859+lapolis@users.noreply.github.com>
2023-10-17 21:19:46 +0100

    Update nessusToExcel.py
    Improved and renamed the WMI tab. It also include Unix cred patch now.

--- nessusToExcel.py
+++ nessusToExcel.py
@@ -58,7 +58,7 @@ def extractAll():
     extractUnsupportedOperatingSystems()
     extractWeakServicePermissions()
     extractWeakSSHAlgorithms()
-    extractWMIAvailable()
+    extractCredPatch()
 
 # Extract system information
 def extractHosts():
@@ -541,8 +541,12 @@ def extractMSPatches():
 
             if (plugin_family == "Windows : Microsoft Bulletins") and (plugin_name != "Microsoft Windows Summary of Missing Patches") and (plugin_name != "Microsoft Patch Bulletin Feasibility Check"):
                 output = nfr.plugin.plugin_output(root, report_host, plugin_id)
-
                 tableData.append((report_fqdn,report_ip,risk_factor,plugin_name,output.strip()))
+
+            elif (plugin_family == "Windows") and (plugin_name.startswith('Security Updates for ')):
+                output = nfr.plugin.plugin_output(root, report_host, plugin_id)
+                tableData.append((report_fqdn,report_ip,risk_factor,plugin_name,output.strip()))
+
            
     if len(tableData) > 0:
         MSPatchesWorksheet = CreateWorksheet(workbook,'Missing Microsoft Patches')
@@ -580,7 +584,7 @@ def extractLinuxPatches():
             risk_factor = nfr.plugin.report_item_value(plugin, 'risk_factor')
             plugin_family = nfr.plugin.report_item_value(plugin, 'pluginFamily')
 
-            if (risk_factor != 'None') and (plugin_family != 'Windows : Microsoft Bulletins'):
+            if (risk_factor != 'None') and (not plugin_family.startswith('Windows')):
                 report_ip = nfr.host.resolved_ip(report_host)
                 report_fqdn = Hosts[report_ip]
                 plugin_name = nfr.plugin.report_item_value(plugin, 'pluginName')
@@ -723,11 +727,11 @@ def extractOutdatedSoftware():
             # Remove all info and MS Patching issues
             risk_factor = nfr.plugin.report_item_value(plugin, 'risk_factor')
             plugin_family = nfr.plugin.report_item_value(plugin, 'pluginFamily')
+            plugin_name = nfr.plugin.report_item_value(plugin, 'pluginName')
 
-            if (risk_factor != 'None') and (plugin_family != 'Windows : Microsoft Bulletins'):
+            if (risk_factor != 'None') and (plugin_family != 'Windows : Microsoft Bulletins') and not plugin_name.startswith('Security Updates for '):
                 report_ip = nfr.host.resolved_ip(report_host)
                 report_fqdn = Hosts[report_ip]
-                plugin_name = nfr.plugin.report_item_value(plugin, 'pluginName')
                 plugin_id = int(nfr.plugin.report_item_value(plugin, 'pluginID'))
                 plugin_output = nfr.plugin.plugin_outputs(root, report_host, plugin_id)
 
@@ -874,9 +878,7 @@ def extractUnsupportedOperatingSystems():
             if 'Microsoft Windows Server 2003' in report_host_os:
                 tableData.append((report_fqdn,report_ip,report_host_os,"13 July 2010","14 July 2015",""))
             if 'Microsoft Windows Server 2008' in report_host_os:
-                tableData.append((report_fqdn,report_ip,report_host_os,"13 January 2015","14 January 2020","10 January 2023"))
-            if 'Microsoft Windows Server 2012' in report_host_os:
-                tableData.append((report_fqdn,report_ip,report_host_os,"09 October 2018","10 October 2023","13 October 2026"))   
+                tableData.append((report_fqdn,report_ip,report_host_os,"13 January 2015","14 January 2020","10 January 2023"))            
             if 'Microsoft Windows XP' in report_host_os:
                 tableData.append((report_fqdn,report_ip,report_host_os,"14 April 2009","08 April 2014",""))
             if 'Microsoft Windows Vista' in report_host_os:
@@ -1089,8 +1091,8 @@ def extractWeakSSHAlgorithms():
     if args.verbose:
         print (f'DEBUG - Completed Weak SSH Algorithms and Ciphers. {len(tableData)} rows took {toc - tic:0.4f} seconds')
 
-# Extract list of hosts with WMI Available (Cred Patch Possible/Successful)
-def extractWMIAvailable():
+# Extract list of hosts in which Cred Patch was Possible/Successful
+def extractCredPatch():
     tic = time.perf_counter()
 
     # Create worksheet with headers. Xlswriter doesn't support autofit so best guess for column widths
@@ -1104,22 +1106,32 @@ def extractWMIAvailable():
     for report_host in nfr.scan.report_hosts(root):
         report_items_per_host = nfr.host.report_items(report_host)
 
-        for report_item in report_items_per_host:
-            plugin_id = int(nfr.plugin.report_item_value(report_item, 'pluginID'))
+        plugin_ids = [int(nfr.plugin.report_item_value(report_item, 'pluginID')) for report_item in report_items_per_host]
 
-            # Filtering pluign "WMI Available"
-            if plugin_id == 24269:
-                report_ip = nfr.host.resolved_ip(report_host)
-                report_fqdn = Hosts[report_ip]
-                report_host_os = nfr.host.detected_os(report_host)
-                
-                if (report_host_os is None or report_host_os.count('\n') > 0):
-                    report_host_os = ""
+        # Filtering pluign "WMI Available"
+        if 24269 in plugin_ids:
+            report_ip = nfr.host.resolved_ip(report_host)
+            report_fqdn = Hosts[report_ip]
+            report_host_os = nfr.host.detected_os(report_host)
+            
+            if (report_host_os is None or report_host_os.count('\n') > 0):
+                report_host_os = ""
 
-                tableData.append((report_fqdn,report_ip,report_host_os,'WMI Available'))
+            tableData.append((report_fqdn,report_ip,report_host_os,'WMI Available (Windows  CredPAtch)'))
+
+        # Filtering pluign "Patch Report" expluding hosts with "WMI Available"
+        elif 66334 in plugin_ids:
+            report_ip = nfr.host.resolved_ip(report_host)
+            report_fqdn = Hosts[report_ip]
+            report_host_os = nfr.host.detected_os(report_host)
+            
+            if (report_host_os is None or report_host_os.count('\n') > 0):
+                report_host_os = ""
+
+            tableData.append((report_fqdn,report_ip,report_host_os,'Patch Report Available (Linux CredPAtch)'))
 
     if len(tableData) > 0:
-        WMIAvailableWorksheet = CreateWorksheet(workbook,'WMI Available')
+        WMIAvailableWorksheet = CreateWorksheet(workbook,'CredPatch Hosts')
         CreateSheetTable(columns,WMIAvailableWorksheet)
         AddTableData(tableData,WMIAvailableWorksheet)
     
@@ -1290,7 +1302,7 @@ unencrypted      = Unencrypted protocols in use. FTP, Telnet etc.
 unquoted         = Unquoted service paths and their weak permissions
 unsupported      = Unsupported operating systems
 winpatches       = Missing Microsoft security patches
-wmi              = Hosts with WMI Available
+credpatch        = Extract all hosts that had a Cred Patch audit done
 '''))
 
 # Keep a timer to keep an eye on performance
@@ -1429,8 +1441,8 @@ else:
                 searchPlugins(args.keyword)
             else: 
                 raise ValueError("Search module requires a keyword")
-        if ('wmi' == module.lower()):
-            extractWMIAvailable()
+        if ('credpatch' == module.lower()):
+            extractCredPatch()
         else:
             print(f'WARN - provided module "{module}" is invalid. Omitting')     
 
